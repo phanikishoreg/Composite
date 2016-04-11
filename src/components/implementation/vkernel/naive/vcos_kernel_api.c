7bd1a459e0f0b7e22a142f3eab0f0bf87a000add
Teo Georgiev <tgeorgiev@gwu.edu>
2016-04-11 14:49:23 -0400

    Making sinv less broken

--- src/components/implementation/vkernel/naive/vcos_kernel_api.c
+++ src/components/implementation/vkernel/naive/vcos_kernel_api.c
@@ -31,9 +31,7 @@ int call_cap_mb(u32_t cap_no, int arg1, int arg2, int arg3)
 	 * function call into another component, with odd calling
 	 * conventions.
 	 */
-	printc("a %d %d\n", cap_no, COS_CAPABILITY_OFFSET);
 	cap_no = (cap_no + 1) << COS_CAPABILITY_OFFSET;
-	printc("b %d\n", cap_no);
 
 	__asm__ __volatile__( \
 		"pushl %%ebp\n\t" \
@@ -58,33 +56,22 @@ vcos_thd_alloc(struct cos_compinfo *ci, compcap_t comp, cos_thd_fn_t fn, void *d
 	 * So we allocate in the vKernel and then move over to the virtualized
 	 * component
 	 */
+	assert(ci);
 
-	// make a new component until we have the virtualized component
-	compcap_t new_comp = cos_comp_alloc(&vkern_info, vkern_info.captbl_cap, vkern_info.pgtbl_cap, (vaddr_t) NULL);
-	assert(new_comp > 0);
-	 
 	// sinv to method that calls cos_thd_alloc (or cos_thd_alloc itself?)
 	// then that does an sinv back
-	sinvcap_t ic = cos_sinv_alloc(&vkern_info, new_comp, (vaddr_t) __inv_vkern_thd_alloc);
+	sinvcap_t ic = cos_sinv_alloc(&vkern_info, comp, (vaddr_t) __inv_vkern_thd_alloc);
 	assert(ic > 0);
-	//sinvcap_t ic2 = cos_cap_copy(&vkern_info, ic, CAP_COMP, &new_comp);
-	//assert(ic2);
 
-	printc("1.5\n");
 	unsigned int r = call_cap_mb(ic, 1, 2, 3);
 	printc("sinchronous invocation %d\n", r);
 	
 	thdcap_t sthd = cos_thd_alloc(&vkern_info, comp, fn, data);
-	printc("2\n");
+	assert(sthd);
 
 	// then proceed as normal
-	assert(sthd);
 	thdcap_t dthd = sthd;
 	cos_cap_init(&vkern_info, sthd, ci, sthd);
-	//thdcap_t dthd = cos_cap_copy(&vkern_info, sthd, CAP_THD, ci);
-	//assert(dthd);
-	/* FIXME: Doesn't work without copying this cap back to vkern! */
-	//cos_cap_init(ci, dthd, &vkern_info, dthd);
 
 	printc("sthd: %ld, dthd: %ld\n", (long int) sthd, (long int) dthd);
 	
